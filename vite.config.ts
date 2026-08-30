f9aef6b33f4977a46941ef3468087d0945bd02d8
Jonathan Lopes <jdmlopes97@gmail.com>
2026-08-30 12:07:19 -0300

    fixed bug for dev build

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,17 @@ import { defineConfig, loadEnv } from 'vite'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  const apiTarget = env.VITE_API_URL || 'http://localhost:5257'
 
   return {
     plugins: [react()],
     server: {
+      host: '0.0.0.0',
       proxy: {
         '/api': {
-          target: env.VITE_API_URL,
+          target: apiTarget,
           changeOrigin: true,
+          secure: false,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
